50c29a974b96cbdabdddb5b2feb89641df0e9c95
Gizen460 <fu15tennis68ku@gmail.com>
2022-12-17 16:45:47 +0900

    ・ゲームポイントカウントアップ機能 ・ゲーム数カウントアップ機能 ・軽く見た目整形

---
+++ script/script.js
@@ -0,0 +1,41 @@
+new Vue({
+  el: '#counter',
+  data() {
+    return {
+      player1Name: 'John',
+      player2Name: 'Mike',
+      player1Count: 0,
+      player2Count: 0,
+      player1GameCount: 0,
+      player2GameCount: 0,
+    };
+  },
+  methods: {
+    getPoint1() {
+      this.player1Count += 15;
+    },
+    getPoint2() {
+      this.player2Count += 15;
+    },
+    game1() {
+      this.player1GameCount++;
+      this.resetGame();
+    },
+    game2() {
+      this.player2GameCount++;
+      this.resetGame();
+    },
+    resetGame() {
+      this.player1Count = 0;
+      this.player2Count = 0;
+    }
+  },
+  computed: {
+    player1WonGame() {
+      return this.player1Count >= 50;
+    },
+    player2WonGame() {
+      return this.player2Count >= 50;
+    }
+  }
+});
